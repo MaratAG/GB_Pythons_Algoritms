f16bb3d0842defb25355fe67ffeb9cb869b9f29e
MaratAG <marat@genatulin.com>
2019-05-23 21:58:42 +0300

    Task 3 for lesson 2 completed.

---
+++ les_2_task_3.py
@@ -0,0 +1,28 @@
+"""
+Задача 3.
+Сформировать из введенного числа обратное по порядку входящих в него цифр и вывести на экран.
+Например, если введено число 3486, надо вывести 6843.
+"""
+
+
+def get_reverse_digits(digits, initial_len):
+    dec = 10
+    reverse_digits = 0
+    len_digits = len(str(digits))
+
+    if len_digits == 1:
+        return digits * (dec ** (initial_len - 1))
+
+    dec_digits = dec ** (len_digits - 1)
+    digit = digits // dec_digits
+
+    reverse_digits += (digit * (dec ** (initial_len - len_digits))
+                       + get_reverse_digits(digits - digit * dec_digits, initial_len))
+    return reverse_digits
+
+
+digits = int(input('Введите натуральное число: '))
+initial_len = len(str(digits))
+reverse_digits = get_reverse_digits(digits, initial_len)
+
+print('Введенное число {}  - обратное число {}'.format(digits, reverse_digits))
