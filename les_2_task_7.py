6e3ce40fcc33911c908e7923d51fc9b1f7de590b
MaratAG <marat@genatulin.com>
2019-05-25 17:02:47 +0300

    Task 7 for lesson 2 completed.

--- les_2_task_7.py
+++ les_2_task_7.py
@@ -18,5 +18,9 @@ def get_result_of_formula(number):
 
 
 number = int(input('Введите любое натуральное число: '))
-print('Сумма чисел ряда 1 .. {}: {}'.format(number, get_sum_of_range(number)))
-print('Результат вычисления по формуле {0} * ({0} + 1) / 2: {1} '.format(number, get_result_of_formula(number)))
+sum_of_range = get_sum_of_range(number)
+result_of_formula = get_result_of_formula(number)
+
+print('Сумма чисел ряда 1 .. {}: {}'.format(number, sum_of_range))
+print('Результат вычисления по формуле {0} * ({0} + 1) / 2: {1} '.format(number, result_of_formula))
+print('Равенство выполняется: {}'.format(sum_of_range == result_of_formula))
