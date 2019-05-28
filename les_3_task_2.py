585fb96d75ff47427bcfc9ed7c1eed550050378d
MaratAG <marat@genatulin.com>
2019-05-28 18:01:41 +0300

    Task 2 for lesson 3 completed.

---
+++ les_3_task_2.py
@@ -0,0 +1,23 @@
+"""
+Задача 2. Во втором массиве сохранить индексы четных элементов первого массива.
+Например, если дан массив со значениями 8, 3, 15, 6, 4, 2, второй массив надо заполнить значениями 1, 4, 5, 6
+(или 0, 3, 4, 5, если индексация начинается с нуля), т.к. именно в этих позициях первого массива стоят четные числа.
+"""
+
+
+import random
+
+
+def get_random_list():
+    return [random.randint(START_RANDOM, FINISH_RANDOM) for _ in range(SIZE)]
+
+
+SIZE = 10
+START_RANDOM = 1
+FINISH_RANDOM = 1000
+
+current_list = get_random_list()
+result_list = [index for index in range(len(current_list)) if current_list[index] % 2 == 0]
+
+print('Исходный массив {}:'.format(current_list))
+print('Массив индексов четных элементов исходного массива {}: '.format(result_list))
